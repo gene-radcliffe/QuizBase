e7eeb5878b41bbbec5c0f2585afe876079728b2c
Laura <laura.s.chavez@outlook.com>
2018-09-21 16:04:56 -0400

    users login

--- backend/app/controllers/login_controller.rb
+++ backend/app/controllers/login_controller.rb
@@ -5,23 +5,23 @@ include ActionController::HttpAuthentication::Basic::ControllerMethods
     def index
         authenticate_or_request_with_http_basic do |username, password|
 
-            user = User.find_by_username(usersname)
+            user = User.find_by_username(username)
 
             if user && user.authenticate(password) then
-                render :json =>
+                render :json =>{
                 :status => :ok,
                 :id => user.id,
                 :admin => user.admin,
                 :name => user.name,
-                :token => user.token
-                :joined => user.create_at
-
+                :token => user.api_token,
+                :joined => user.created_at
+            }
             else
-                render :json =>(
+                render :json =>{
                 :status => :unauthorized,
                 :message => "unauthorized"
-                )
-                
+            }
+
             end
         end
 
