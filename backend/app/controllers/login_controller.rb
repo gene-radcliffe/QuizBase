a2ac397cab9873415e741d666645619cd4183650
Laura <laura.s.chavez@outlook.com>
2018-09-21 15:50:02 -0400

    login

--- backend/app/controllers/login_controller.rb
+++ backend/app/controllers/login_controller.rb
@@ -1,2 +1,30 @@
 class LoginController < ApplicationController
+include ActionController::HttpAuthentication::Basic::ControllerMethods
+
+
+    def index
+        authenticate_or_request_with_http_basic do |username, password|
+
+            user = User.find_by_username(usersname)
+
+            if user && user.authenticate(password) then
+                render :json =>
+                :status => :ok,
+                :id => user.id,
+                :admin => user.admin,
+                :name => user.name,
+                :token => user.token
+                :joined => user.create_at
+
+            else
+                render :json =>(
+                :status => :unauthorized,
+                :message => "unauthorized"
+                )
+                
+            end
+        end
+
+    
+    end
 end
