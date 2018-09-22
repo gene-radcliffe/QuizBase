3160bcfa7366a35483799eb9363f6d0be84c9d89
gene-radcliffe <g_radcliffe@hotmail.com>
2018-09-21 22:35:11 -0400

    reconfiguring cors

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -32,12 +32,12 @@ module Backend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-       origins '*'
-       resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :put, :options]
-      end
-     end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #    origins '*'
+    #    resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :put, :options]
+    #   end
+    #  end
      
   end
 end
