fa7276000a4ec488cb1586b42f1ec3ee08406ad6
josephrfarmer <josephr.farmer24@gmail.com>
2018-09-22 13:04:52 -0400

    rearranged folders and routes

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
     
     def index
         @users = User.all 
-        render 'app/views/users/index.json'
     end 
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :users
-resources :login 
-resources :registers
-resources :quizzes
+  namespace :api do
+    resources :users
+    resources :login 
+    resources :registers
+    resources :quizzes
+  end
 end
